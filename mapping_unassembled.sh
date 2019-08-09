59d888efd8d081b6357995ffe3b5d42547545f45
Alex Wilcox <41028740+awilcox83@users.noreply.github.com>
2019-08-09 11:29:47 -0700

    Update mapping_unassembled.sh

--- mapping_unassembled.sh
+++ mapping_unassembled.sh
@@ -1,4 +1,4 @@
-#This script takes all fastq files that did not assemble and maps them to all assemblies to find the closes match.
+#This script takes all fastq files that did not assemble and maps them to all assemblies to find the closest match.
 
 
 #Index all fastas for mapping
@@ -46,4 +46,30 @@ do
         bowtie2 -x ${y}_ref -U ${prefix}R1.unpaired.qc.fastq -U ${prefix}R2.unpaired.qc.fastq -1 ${prefix}R1.qc.fastq -2 ${prefix}R2.qc.fastq -S ${prefix}.mapped.SAM 2>> bowtie_${prefix}.txt
         printf "\n\n\n" >> bowtie_${prefix}.txt
     done
+done
+
+
+rm phage_mapping.tsv
+touch phage_mapping.tsv
+printf "sample\t105b\t105c\t105d\t120a\t120c\t28d\t28i\t42c\t42d\t42e\t42f\t42g\t42k\t42m\t64505\t64506\t64507\t64511\t85a\t85c\t85d_phi2954\t85d_phi6\t90a\t90b\tca51\tca58\tca61\tca62\tca64\tca64d\tca65\tca65c_1\tca65d\tca66\tca66d\tca68c\tca69\tca73\tca76\tkri289\tkri301\tphi10\tphi14\tphi6\tphi9\tpt106\tpt110\tv1-1\tv1-3\tv1-7\tv1_4_phi2954\tv1_4_phi6\tv2\tv3\n" > phage_mapping.tsv
+
+# Create a table showing how well each sample maps to each reference genome/assembly 
+
+for file in bowtie*.txt
+do
+   #Get sample name from filename
+	A="$(cut -d'_' -f2 <<<$file)"
+	printf $A >> phage_mapping.tsv
+	printf "\t" >> phage_mapping.tsv
+   #get % from all lines containing "overall" 
+    cat $file | while read line
+    do
+        if [[ $line == *"overall"* ]]
+        then
+            printf  $line | cut -d' ' -f1 >> phage_mapping.tsv
+            #errors because printf treats % as a special character, but it doesn't affect the output
+            printf "\t" >> phage_mapping.tsv
+        fi
+    done
+    printf "\n" >> phage_mapping.tsv
 done
