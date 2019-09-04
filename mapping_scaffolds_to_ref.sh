7a3c23e3ef10bb15948ae0ce307abc7638ea04c9
Alex Wilcox <41028740+awilcox83@users.noreply.github.com>
2019-09-04 15:47:22 -0700

    Create mapping_scaffolds_to_ref.sh

---
+++ mapping_scaffolds_to_ref.sh
@@ -0,0 +1,5 @@
+# using bwa-mem to map the scaffolds against reference genomes.
+
+# I will do each one manually because each assembly will probably require a different reference.
+
+bwa mem 28d.fasta 90a.fasta
